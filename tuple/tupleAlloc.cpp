2a4866deacd1cd4745d03655aab899dc7caec785
Jeffrey Kelling <j.kelling@hzdr.de>
2019-11-08 14:58:54 +0100

    tupleAlloc: Add omp_target_memcpy

--- tuple/tupleAlloc.cpp
+++ tuple/tupleAlloc.cpp
@@ -6,27 +6,37 @@
 int main()
 {
 	//tuple<unsigned int *, unsigned int *, unsigned int *, unsigned long>
-	int A=1;
+	int A[] = {10,2,3,4};
         int *tA = (int *) omp_target_alloc(4, 0);
         int *tB = (int *) omp_target_alloc(4, 0);
         int *tC = (int *) omp_target_alloc(4, 0);
-	auto argsD = std::make_tuple(tA, tB, tC, A);
+
+	omp_target_memcpy(tA, A, 4*sizeof(int), 0, 0, 0, omp_get_initial_device());
+
+	auto argsD = std::make_tuple(tA, tB, tC, A[2]);
 	printf("HOST: tA=%#llx, tB=%#llx, tC=%#llx, A=%d\n"
 		, (unsigned long long int)(std::get<0>(argsD)),(unsigned long long int)(std::get<1>(argsD)),(unsigned long long int)(std::get<2>(argsD))
 		, int(std::get<3>(argsD)));
-        int thD = -1;
 
-#pragma omp target map(from:thD)
+#pragma omp target map(to:argsD)
 	{
 #pragma omp teams num_teams(1) thread_limit(4)
 		{
-			printf("DEVICE: tA=%#llx, tB=%#llx, tC=%#llx, A=%d\n"
+			printf("DEVICE: tA=%#llx, tB=%#llx, tC=%#llx, A=%d, A[0]=%d\n"
 				, (unsigned long long int)(std::get<0>(argsD)),(unsigned long long int)(std::get<1>(argsD)),(unsigned long long int)(std::get<2>(argsD))
-				, int(std::get<3>(argsD)));
+				, int(std::get<3>(argsD))
+				, std::get<0>(argsD)[0]
+				);
                     int * td = (int*)(std::get<0>(argsD));
-                    *td = 12;
-                    thD = *td;
+                    int * tdC = (int*)(std::get<2>(argsD));
+		    if(omp_get_thread_num()==0)
+			    *td *= 12;
+		    for(int a = 0; a < 4; ++a)
+			    tdC[a] = *td;
 		}
 	}
-       printf("thD=%d\n", thD);
+
+	int C[4];
+	omp_target_memcpy(C, tC, 4*sizeof(int), 0, 0, omp_get_initial_device(), 0);
+	printf("C[0]=%d\n", C[0]);
 }
