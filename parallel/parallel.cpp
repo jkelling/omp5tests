6a6dd31023a1006b66c177b94a524655c79cf374
Jeffrey Kelling <j.kelling@hzdr.de>
2019-12-16 09:13:26 -0500

    parallel: Add compile-time switch for ibm XL

--- parallel/parallel.cpp
+++ parallel/parallel.cpp
@@ -7,7 +7,7 @@
 int main()
 {
 	int maxThreads = omp_get_max_threads();
-	int numTeams = 2;
+	int numTeams = 4;
 
 	std::cout << "maxThreads=" << maxThreads
 		<< "\tnumTeams=" << numTeams << std::endl;
@@ -19,7 +19,11 @@ int main()
 			for(int t = 0; t < omp_get_num_teams(); ++t)
 			{
 				printf("TEAM %d, t=%d\n", (int)(omp_get_team_num()), t);
-#pragma omp parallel num_threads(maxThreads)
+#ifndef __ibmxl_vrm__
+				// setting num_threads to any value leads XL to run only one thread per team
+				omp_set_num_threads(maxThreads);
+#endif
+#pragma omp parallel //num_threads(maxThreads)
 				{
 					printf("TEAM %d, THREAD=%d, maxThreads=%d\n"
 						, (int)(omp_get_team_num()), (int)(omp_get_thread_num()), maxThreads);
